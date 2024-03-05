1a128b7232d5807e047e7ec832934eac4faa71d4
Abhisheksingh-2 <Abhisheks8319@gmail.com>
2024-03-05 16:21:36 +0530

    My first react

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,16 @@
+
+
+
+
+
+function App() {
+  
+
+  return (
+   <h1> React learn with Vite  | ABHISHEK
+<p> Vite is a build tool that aims to provide a faster and leaner devlopment experience for modern web projects.</p>
+   </h1>
+  )
+}
+
+export default App
