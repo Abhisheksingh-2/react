43b04d9dc0ab0c4afc1ac1e43fcb1a5c164bc887
Abhisheksingh-2 <Abhisheks8319@gmail.com>
2024-03-07 17:25:47 +0530

    Main

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-
-// function Myapp(){
-//     return (
-//         <>
-//         <h1>My app is the best app</h1>
-//         <p>Hello, Everyone| ABHISHEK SINGH</p>
-//         </>
-//     )
-// }
+/*
+function Myapp(){
+    return (
+        <>
+        <h1>My app is the best app</h1>
+        <p>Hello, Everyone| ABHISHEK SINGH</p>
+        </>
+    )
+}
 const reactElement={
     type:'a',
        props:{
@@ -19,16 +19,23 @@ const reactElement={
     },
     children:'Click me to visit google',
     
-}
+}*/
 
 const anotherElement=(
     <a href="https://google.com" target="_blank">Visit the google</a>
 )
+const anotherUser='Golu123'
+  const reactElement=React.createElement(
+    'a',
+    {href:'https://google.com',target:'_blank'},
+    'Click me to visit Google',
+    anotherUser
+  )
 
-
+  
 ReactDOM.createRoot(document.getElementById('root'))
 .render(
+     reactElement
  
-    anotherElement 
   
 )
