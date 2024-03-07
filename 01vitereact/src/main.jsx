847ec80939227f9244f5a81f75bf8441438d9b01
Abhisheksingh-2 <Abhisheks8319@gmail.com>
2024-03-07 16:59:17 +0530

    Main

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,10 +2,33 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+// function Myapp(){
+//     return (
+//         <>
+//         <h1>My app is the best app</h1>
+//         <p>Hello, Everyone| ABHISHEK SINGH</p>
+//         </>
+//     )
+// }
+const reactElement={
+    type:'a',
+       props:{
+       href:'https://google.com',
+       target:'_blank'
+       
+    },
+    children:'Click me to visit google',
+    
+}
+
+const anotherElement=(
+    <a href="https://google.com" target="_blank">Visit the google</a>
+)
+
 
 ReactDOM.createRoot(document.getElementById('root'))
 .render(
  
-    <App />,
+    anotherElement 
   
 )
