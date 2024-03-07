392906f0a2775a1dc3059592f309bf5686afba8e
Abhisheksingh-2 <Abhisheks8319@gmail.com>
2024-03-07 17:26:14 +0530

    App

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -2,10 +2,11 @@ import Chai from "./chai"
 
 function App() {
   
+const username='ABHISHEK SINGH'
 
   return (
    <>
-   <h1> React learn with Vite  | ABHISHEK</h1>
+   <h1> React learn with Vite  | {username}</h1>
 <p> Vite is a build tool that aims to provide a faster and leaner devlopment experience for modern web projects. 
   It consists of two major parts:
 
@@ -15,7 +16,7 @@ function App() {
 * A build command that bundles your code with Rollup, pre-configured to output highly 
 optimized static assets for production.
 
-Vite is opinionated and comes with sensible defaults out of the box. Read about what's possible in the Features Guide. 
+Vite is opinionated and comes with sensible defaults out of the box. Read about what  possible in the Features Guide. 
 Support for frameworks or integration with other tools is possible through Plugins.</p>
 
    <Chai/>
