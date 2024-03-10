18ede0f9e35434b546ef843dcbf1349a18a593e5
Abhisheksingh-2 <Abhisheks8319@gmail.com>
2024-03-10 09:15:28 +0530

    App

--- 02ReactLearn/src/App.jsx
+++ 02ReactLearn/src/App.jsx
@@ -5,12 +5,12 @@ import './App.css'
 
 function App() {
 
-  let [counter,setCounter]=useState(10)
+   let [counter,setCounter]=useState(10)
  
 //let counter=10;
 
 const IncreaseValue=()=>{
-  if(setCounter<=20){
+  if(counter<20){
 
 
   setCounter(counter+1);
@@ -23,7 +23,7 @@ const IncreaseValue=()=>{
 const DecreaseValue=()=>{
 
   
-  if(!setCounter==0){
+  if(counter>0){
     setCounter(counter-1);
   }
 // console.log("Decrease Value: ",counter);
