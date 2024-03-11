4a8f065c34a32788950556833f2945886e9f547b
Abhisheksingh-2 <Abhisheks8319@gmail.com>
2024-03-11 17:26:32 +0530

    Card

---
+++ 03TailwindProps/src/components/Card.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Card({username,btnText}){
+    console.log(username);
+
+    return (
+        
+        <div className="w-[300px] rounded-md border">
+        <img
+          src="https://images.unsplash.com/photo-1522199755839-a2bacb67c546?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGJsb2d8ZW58MHx8MHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=60"
+          alt="Laptop"
+          className="h-[200px] w-full rounded-md object-cover"
+        />
+        <div className="p-4">
+          <h1 className="text-lg font-semibold">{username}</h1>
+          <p className="mt-3 text-sm text-gray-600">
+   
