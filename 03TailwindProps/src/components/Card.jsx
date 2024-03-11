9d3937b88cc33b89b44bcbd814cace0f96da3258
Abhisheksingh-2 <Abhisheks8319@gmail.com>
2024-03-11 17:26:44 +0530

    Card

--- 03TailwindProps/src/components/Card.jsx
+++ 03TailwindProps/src/components/Card.jsx
@@ -15,3 +15,19 @@ function Card({username,btnText}){
           <h1 className="text-lg font-semibold">{username}</h1>
           <p className="mt-3 text-sm text-gray-600">
    
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
+            debitis?
+          </p>
+          <button
+            type="button"
+            className="mt-4 rounded-sm bg-black px-2.5 py-1 text-[10px] font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
+          >
+            {btnText}
+          </button>
+        </div>
+      </div>
+    )
+}
+
+
+export default Card
